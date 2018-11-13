31dd758045991c336db8e134ad24f20396c9cd72
hj0503 <857131753@qq.com>
2018-11-13 23:37:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd'
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">hello world</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
