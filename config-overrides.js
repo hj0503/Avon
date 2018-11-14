b52c577b1d71ed6fffff0a512e133f8cd5d14b06
hj0503 <857131753@qq.com>
2018-11-15 00:37:49 +0800

    add less plugin

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const tsImportPluginFactory = require('ts-import-plugin')
 const { getLoader } = require("react-app-rewired");
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
   const tsLoader = getLoader(
@@ -15,10 +16,14 @@ module.exports = function override(config, env) {
       before: [ tsImportPluginFactory({
         libraryDirectory: 'es',
         libraryName: 'antd',
-        style: 'css',
+        style: true,
       }) ]
     })
   };
+
+  config = rewireLess.withLoaderOptions({
+      modifyVars: { "@primary-color": "#1DA57A" },
+  })(config, env)
 
   return config;
 }
