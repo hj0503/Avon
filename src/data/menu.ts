7d885f9f2bdf4a13fd6c79f03c57497de43f5ed7
hj0503 <857131753@qq.com>
2018-12-03 00:30:26 +0800

    add menu_list type

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,4 +1,24 @@
-export const MENU_LIST = [
+export type menu = {
+  icon: string,
+  label: string,
+  key: string,
+}
+
+export type subMenu = {
+  icon: string,
+  label: string,
+  key: string,
+  subMenu?: Array<menu>
+}
+
+export type menu_list = {
+  icon: string,
+  label: string,
+  key: string,
+  subMenu?: Array<subMenu>
+}
+
+export const MENU_LIST: Array<menu_list> = [
   {
     icon: "user",
     label: "test1",
