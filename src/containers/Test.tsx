e4b6332c74ae1105ac66b70ea920073ed81c7fa6
hj0503 <857131753@qq.com>
2018-12-20 23:47:38 +0800

    add containers

---
+++ src/containers/Test.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+  render() {
+    return (
+      <div>
+        含有connect的全局组件放在这个文件夹
+      </div>
+    )
+  }
+}
