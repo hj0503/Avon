f1c3d4621260b8368d33eb7ed28fd00d21ea0ca6
hj0503 <857131753@qq.com>
2019-01-10 23:42:31 +0800

    add form rules

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,47 +1,104 @@
 import React, { PureComponent } from "react";
-import Login from "./Login";
-import { withRouter, RouteComponentProps } from "react-router";
-import { logout } from "src/api/authentication";
-import md5 from "js-md5";
-import { fetchUserInfo } from "src/reducers/authReducer";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+import "./index.less";
+import { WrappedFormUtils } from "antd/lib/form/Form";
 import { connect } from "react-redux";
+import { fetchUserInfo } from "src/reducers/authReducer";
+import { withRouter, RouteComponentProps } from "react-router";
+import md5 from "js-md5";
 
-interface Props extends RouteComponentProps {
+const FormItem = Form.Item;
+
+interface LoginProps extends RouteComponentProps {
   history;
-  fetchUserInfo;
+  form: WrappedFormUtils;
+  fetchUserInfo: (params) => Promise<any>;
 }
 
-class Index extends PureComponent<Props> {
+class Login extends PureComponent<LoginProps> {
   readonly state = {
     loginLoading: false
   };
-  onLogin = async (username, password) => {
-    const { history } = this.props;
-    this.setState({ loginLoading: true });
-    const params = {
-      username,
-      password: md5(password),
-      remeberMe: "true"
-    };
-    this.props.fetchUserInfo(params).then(() => {
-      this.setState({
-        loginLoading: false
-      });
-      history.push("/index");
+
+  handleLogin = e => {
+    const {history, form} = this.props
+    e.preventDefault();
+    form.validateFields((err, values) => {
+      if (!err) {
+        this.setState({
+          loginLoading: true
+        })
+        const {username, password} = values
+          const params = {
+            username,
+            password: md5(password),
+            remeberMe: "true"
+          };
+        this.props.fetchUserInfo(params).then(() => {
+          this.setState({
+            loginLoading: false
+          });
+          history.push("/index");
+        });
+      }
     });
   };
 
-  onLogout = () => {
-    logout();
+  onChange = event => {
+    const e = event.target;
+    this.setState({
+      [e.name]: e.value
+    });
   };
   render() {
+    const {
+      form: { getFieldDecorator }
+    } = this.props;
     const { loginLoading } = this.state;
     return (
-      <Login
-        loginLoading={loginLoading}
-        onLogin={this.onLogin}
-        onLogout={this.onLogout}
-      />
+      <Form className="login-form" onSubmit={this.handleLogin}>
+        <FormItem>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "请输入你的用户名" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              name="username"
+              type="test"
+              onChange={this.onChange}
+              placeholder="Username"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "请输入你的密码" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              name="password"
+              type="password"
+              onChange={this.onChange}
+              placeholder="Password"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          <Checkbox>Remember me</Checkbox>
+          <a className="login-form-forgot" onClick={() => console.log("test")}>
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            loading={loginLoading}
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
     );
   }
 }
@@ -58,5 +115,5 @@ export default withRouter(
   connect(
     null,
     mapDispatchToProps
-  )(Index)
+  )(Form.create({})(Login))
 );
